c245069015cb1602dacb29d59488226ddc68bf93
Chris Milson <chris.w.milson@gmail.com>
2020-03-24 10:25:24 +0900

    Calculate age with recursion and memoisation

--- src/age.ts
+++ src/age.ts
@@ -3,14 +3,29 @@
  *
  * @param physicalAge The age in years
  */
-export const physicalToMental = (physicalAge: number, base = 10) => {
-  let mentalAge = 0
-  while (physicalAge > 0) {
-    mentalAge += physicalAge % base
-    physicalAge = Math.floor(physicalAge / base)
+export const physicalToMental = ((base = 10) => {
+  const mental: number[] = []
+  for (let i = 0; i < base; i++) {
+    mental[i] = i
   }
-  return mentalAge
-}
+  mental[NaN] = 0
+
+  /** This is recursive */
+  const rec = (physical: number) => {
+    if (!mental[physical] && mental[physical] !== 0) {
+      console.log(`calculating ${physical}, ${mental[physical]}`)
+      mental[physical] = rec(Math.floor(physical / base)) + (physical % base)
+    }
+    return mental[physical]
+  }
+
+  // pre-calculate
+  for (let i = 0; i < 2000; i++) {
+    rec(i)
+  }
+
+  return rec
+})()
 
 /**
  * Returns the smallest number whose sum of digits (defaults to base ten) is the
